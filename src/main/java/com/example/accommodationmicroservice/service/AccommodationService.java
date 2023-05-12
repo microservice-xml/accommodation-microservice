e0ef33a666ff1f4083102930b72d56bdee822122
vukomanv <vladimir.vukoman.v@gmail.com>
2023-05-12 20:44:24 +0200

    Added search accommodation functionality

--- src/main/java/com/example/accommodationmicroservice/service/AccommodationService.java
+++ src/main/java/com/example/accommodationmicroservice/service/AccommodationService.java
@@ -1,11 +1,18 @@
 package com.example.accommodationmicroservice.service;
 
+import com.example.accommodationmicroservice.dto.AccommodationSearchDto;
 import com.example.accommodationmicroservice.model.Accommodation;
 import com.example.accommodationmicroservice.repository.AccommodationRepository;
+import communication.AccommodationServiceGrpc;
+import communication.SearchRequest;
+import communication.SearchResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,5 +25,31 @@ public class AccommodationService {
     }
     public Accommodation addAccommodation(Accommodation accommodation){
         return accommodationRepository.save(accommodation);
+    }
+
+    public List<Accommodation> search(AccommodationSearchDto accommodationSearchDto) {
+        var accommodations = accommodationRepository.findAllByLocationIgnoreCaseAndMinGuestsLessThanEqualAndMaxGuestsGreaterThanEqual(accommodationSearchDto.getLocation(), accommodationSearchDto.getGuestCount(), accommodationSearchDto.getGuestCount());
+        if (accommodations.size() == 0) {
+            return new ArrayList<>();
+        }
+        var accommodationIds = accommodations.stream().map(Accommodation::getId).toList();
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9095)
+                .usePlaintext()
+                .build();
+
+        AccommodationServiceGrpc.AccommodationServiceBlockingStub blockingStub = AccommodationServiceGrpc.newBlockingStub(channel);
+        SearchRequest req = SearchRequest
+                .newBuilder()
+                .setStartYear(accommodationSearchDto.getStart().getYear())
+                .setStartMonth(accommodationSearchDto.getStart().getMonthValue())
+                .setStartDay(accommodationSearchDto.getStart().getDayOfMonth())
+                .setEndYear(accommodationSearchDto.getEnd().getYear())
+                .setEndMonth(accommodationSearchDto.getEnd().getMonthValue())
+                .setEndDay(accommodationSearchDto.getEnd().getDayOfMonth())
+                .addAllAccommodationIds(accommodationIds)
+                .build();
+        SearchResponse response = blockingStub.searchByAvailabilityRange(req);
+        return accommodationRepository.findAllById(response.getAccommodationIdsList());
     }
 }
