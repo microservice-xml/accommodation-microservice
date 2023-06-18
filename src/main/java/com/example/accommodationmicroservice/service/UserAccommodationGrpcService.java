a2200f24a817b6b0df2ed3e7219b6e6165045432
vukomanv <vladimir.vukoman.v@gmail.com>
2023-06-18 14:21:33 +0200

    Added tracing

--- src/main/java/com/example/accommodationmicroservice/service/UserAccommodationGrpcService.java
+++ src/main/java/com/example/accommodationmicroservice/service/UserAccommodationGrpcService.java
@@ -1,16 +1,20 @@
 package com.example.accommodationmicroservice.service;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @GrpcService
 @RequiredArgsConstructor
 public class UserAccommodationGrpcService extends communication.UserAccommodationServiceGrpc.UserAccommodationServiceImplBase{
 
     private final AccommodationService accommodationService;
+    private Logger logger = LoggerFactory.getLogger(UserAccommodationGrpcService.class);
 
     @Override
     public void checkForDelete(communication.UserIdRequest request,
                                io.grpc.stub.StreamObserver<communication.BooleanResponse> responseObserver) {
+        logger.trace("Request to check if user with id {} can be deleted was made", request.getId());
         responseObserver.onNext(accommodationService.CheckForDelete(request.getId()));
         responseObserver.onCompleted();
     }
